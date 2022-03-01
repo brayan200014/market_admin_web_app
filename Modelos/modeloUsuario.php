50b7d5726f67fc87583eea0f82a09ad1a91e313a
Alejandro-AM <78048132+Alejandro-AM@users.noreply.github.com>
2022-03-01 12:17:28 -0600

    Se creo el modelo de usuario

--- Modelos/modeloUsuario.php
+++ Modelos/modeloUsuario.php
@@ -1,5 +1,54 @@
-<php
+<?php
 
+    Class Usuario{
+        private $usuario;
+        private $db;
+
+        public function __construct() {
+            $this->usuario = array();
+            $this->db = new PDO('mysql:host=localhost;dbname=medi', "movil1301", "Movil$1301");
+        }
+
+        private function setNames() {
+            return $this->db->query("SET NAMES 'utf8'");
+        }
+
+        public function getUsuarios() {
+
+            self::setNames();
+            $sql = "SELECT * FROM Usuarios";
+            foreach ($this->db->query($sql) as $res) {
+                $this->usuario[] = $res;
+            }
+            return $this->usuario;
+            $this->db = null;
+        }
+
+        public function setUsuario($usuario, $correo, $clave, $idEmpleado) {
+
+            self::setNames();
+            $sql = "INSERT INTO Usuarios (NombreUsuario, Correo, Contrasena, FechaCreacion, Estado, Empleados_IdEmpleado)
+                    VALUES ('$usuario','$correo', '$clave', NOW(), true, $idEmpleado);";
+            $result = $this->db->query($sql);
     
+            if ($result) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public function buscarUsuario($id){
+            self::setNames();
+            $sql = "SELECT * FROM Usuarios WHERE IdUsuario = $id";
+            $result = $this->db->query($sql);
+
+            foreach ($this->db->query($sql) as $res) {
+                $this->usuario[] = $res;
+            }
+            return $this->usuario;
+            $this->db = null;
+        }
+    }
 
 ?>
