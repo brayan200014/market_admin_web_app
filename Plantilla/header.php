2933d5212a08ca01faf6f6a098dd46d2ebed71fd
Alejandro-AM <78048132+Alejandro-AM@users.noreply.github.com>
2022-03-06 02:18:20 -0600

    Nombre de usuario dinamico segun el usuario logeado

--- Plantilla/header.php
+++ Plantilla/header.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -202,9 +205,9 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600"><?php echo $_SESSION['nombreUsuario'] ?></span>
                                 <img class="img-profile rounded-circle"
-                                    src="../PLantilla/img/undraw_profile.svg">
+                                    src="../Plantilla/img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
