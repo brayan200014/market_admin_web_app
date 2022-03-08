b179dc5184b686cc6e479d09aacd40a43160d47a
Alejandro-AM <78048132+Alejandro-AM@users.noreply.github.com>
2022-03-07 22:50:02 -0600

    Se corrigió el vinculo en el logo

--- Plantilla/header.php
+++ Plantilla/header.php
@@ -37,7 +37,7 @@ session_start();
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../Paginas/principal.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
