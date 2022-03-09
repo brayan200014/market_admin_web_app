bb3709c933aaeeec65e706f0aab90e54e80b7e62
jete20 <78774120+jete20@users.noreply.github.com>
2022-03-09 13:43:54 -0600

    Avance

--- Vistas/vistaActualizarCompra.php
+++ Vistas/vistaActualizarCompra.php
@@ -50,7 +50,7 @@
             <div class=".container-fluid d-flex flex-wrap justify-content-between" >
                 <div class="mb-3">
                     <label for="IdProducto" class="form-label">ID del Producto</label>
-                    <input type="text" class="form-control needs-validation" id="IdProducto" name="Producto[]"  placeholder="Ingrese el id del Producto" required pattern="[0-9]+">
+                    <input type="text" class="form-control needs-validation" id="IdProducto" name="Producto[]" value="<?php echo $datosDetalleCompra[$i]['Productos_IdProducto']?>" placeholder="Ingrese el id del Producto" required pattern="[0-9]+">
                     <div class="valid-feedback">
                         Formato Valido
                     </div>
@@ -60,7 +60,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="Cantidad" class="form-label">Cantidad</label>
-                    <input type="text" class="form-control needs-validation" id="Cantidad" name="Cantidad[]" placeholder="Ingrese la cantidad" required pattern="[0-9]+">
+                    <input type="text" class="form-control needs-validation" id="Cantidad" name="Cantidad[]" value="<?php echo $datosDetalleCompra[$i]['Cantidad'] ?>" placeholder="Ingrese la cantidad" required pattern="[0-9]+">
                     <div class="valid-feedback">
                         Formato Valido
                     </div>
@@ -70,7 +70,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="Precio" class="form-label">Precio</label>
-                    <input type="text" class="form-control needs-validation" id="Precio" name="Precio[]" placeholder="Ingrese el precio" required pattern="^[0-9]+([.][0-9]+)?$">
+                    <input type="text" class="form-control needs-validation" id="Precio" name="Precio[]" value="<?php echo $datosDetalleCompra[$i]['PrecioCompra'] ?>" placeholder="Ingrese el precio" required pattern="^[0-9]+([.][0-9]+)?$">
                     <div class="valid-feedback">
                         Formato Valido
                     </div>
