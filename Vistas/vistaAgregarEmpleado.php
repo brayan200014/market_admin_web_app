c5f089f4b6c6e93691bd69d9631114c12d2fc5ca
[andreaquinto5] <[nikia2323@gmail.com]>
2022-03-09 12:54:44 -0600

    Arreglando cantidad de letras en NombreSucursal y Nombre Empleados

--- Vistas/vistaAgregarEmpleado.php
+++ Vistas/vistaAgregarEmpleado.php
@@ -16,13 +16,13 @@
     <form class="g-3 needs-validation" action="../Controladores/controladorEmpleados.php" method="POST" novalidate>
         <div class="m-4 position-relative">
             <label for="nombre" class="form-label">Nombre del empleado: </label>
-            <input type="text" class="form-control needs-validation" id="nombre" name="nombre" placeholder="Ingrese el nombre del empleado" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{3,25}"  size="20" required >
+            <input type="text" class="form-control needs-validation" id="nombre" name="nombre" placeholder="Ingrese el nombre del empleado" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{3,25}"  size="25" required >
             <div class="valid-tooltip">¡Campo válido!</div>
             <div class="invalid-tooltip">Campo no valido.</div>
         </div>
         <div class="m-4 position-relative">
             <label for="apellido" class="form-label">Apellido del empleado: </label>
-            <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Ingrese el apellido del empleado" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{3,25}"  size="20" required>
+            <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Ingrese el apellido del empleado" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{3,25}"  size="25" required>
             <div class="valid-tooltip">¡Campo válido!</div>
             <div class="invalid-tooltip">Campo no valido.</div>
         </div>
@@ -44,7 +44,7 @@
         </div>
         <div class="m-4 position-relative">
             <label for="email" class="form-label">Correo Electrónico: </label>
-            <input type="text" class="form-control" id="email" name="email" placeholder="Ingrese un email válido ejemplo: web@gmail.com" pattern=".+@\.com" size="15" required> <!--Los correos deben ser gmail-->
+            <input type="text" class="form-control" id="email" name="email" placeholder="Ingrese un email válido ejemplo: web@gmail.com" pattern=".+@gmail\.com" size="15" required> <!--Los correos deben ser gmail-->
             <div class="valid-tooltip">¡Campo valido!</div>
             <div class="invalid-tooltip">Campo no valido.</div>
         </div>
